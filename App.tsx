b65d49afd575eb2b2eefbe39e80f57c5e692f535
Pheonix747hs <94136752+Pheonix747hs@users.noreply.github.com>
2024-08-05 21:38:55 +0530

    integrated blender model rendering

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import {
   ViroText,
   ViroTrackingReason,
   ViroTrackingStateConstants,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroOrbitCamera
 } from "@reactvision/react-viro";
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
@@ -14,7 +17,7 @@ const HelloWorldSceneAR = () => {
   function onInitialized(state: any, reason: ViroTrackingReason) {
     console.log("onInitialized", state, reason);
     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText("hopepe");
+      setText("AR Initialized");
     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
       // Handle loss of tracking
     }
@@ -22,11 +25,24 @@ const HelloWorldSceneAR = () => {
 
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroAmbientLight color="#ffffff" intensity={500}/>
       <ViroText
         text={text}
         scale={[0.5, 0.5, 0.5]}
         position={[0, 0, -1]}
         style={styles.helloWorldTextStyle}
+      />
+      {/* <ViroOrbitCamera 
+      position={[0, -1, 0]}
+      focalPoint={[0, 0, -1]}
+      active={true}
+      /> */}
+      <Viro3DObject
+        source={{uri: "https://drive.google.com/uc?export=download&id=1nnluutWBfWQ7OxpUfptNEfRfIHHp0dWW"}} // Adjust the path to your VBX model
+        position={[ 0, -1, -1 ]}
+        scale={[0.1, 0.1, 0.1]}
+        type="GLB"
+
       />
     </ViroARScene>
   );
@@ -48,7 +64,7 @@ var styles = StyleSheet.create({
   f1: { flex: 1 },
   helloWorldTextStyle: {
     fontFamily: "Arial",
-    fontSize: 300,
+    fontSize: 30,
     color: "#ffffff",
     textAlignVertical: "center",
     textAlign: "center",
