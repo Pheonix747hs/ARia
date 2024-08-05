f4d0b31f818aa2e15bccbb6a1c7b3b8d5ed8a81c
Pheonix747hs <94136752+Pheonix747hs@users.noreply.github.com>
2024-08-05 21:54:29 +0530

    reverted gdrive import

--- App.tsx
+++ App.tsx
@@ -38,7 +38,7 @@ const HelloWorldSceneAR = () => {
       active={true}
       /> */}
       <Viro3DObject
-        source={{uri: "https://drive.google.com/uc?export=download&id=1nnluutWBfWQ7OxpUfptNEfRfIHHp0dWW"}} // Adjust the path to your VBX model
+        source={require("./assets/table_scene.glb")} // Adjust the path to your VBX model
         position={[ 0, -1, -1 ]}
         scale={[0.1, 0.1, 0.1]}
         type="GLB"
