44c7f2e0abd42f9fbb97868ca3ac999bbcbe9a45
Pheonix747hs <ojasvinborawke@gmail.com>
2024-09-10 14:12:30 +0530

    cleanup merge

--- App.tsx
+++ App.tsx
@@ -28,33 +28,32 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName="SplashScreen">
-      <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="ARSceneScreen" component={ARSceneScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-    
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroAmbientLight color="#ffffff" intensity={500}/>
-      
+   <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ARSceneScreen" component={ARSceneScreen} />
+        ##
+        <ViroARScene onTrackingUpdated={onInitialized}>
+        <ViroAmbientLight color="#ffffff" intensity={500} />
+
 
         <Viro3DObject
           source={require("./assets/table_scene.glb")}
           scale={[0.1, 0.1, 0.1]}
           position={[0, -0.5, -0.4]}
           type="GLB"
-          rotation={[0, 90, 0]}
-        />
+          rotation={[0, 90, 0]} />
         <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
+          text={text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle} />
 
-    </ViroARScene>
+      </ViroARScene>
+      ###
+      </Stack.Navigator>
+    </NavigationContainer>
 
 
   );
